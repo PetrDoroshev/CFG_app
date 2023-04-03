e9cd15024ef296d8ecad07266052208f83548dbd
PetrDoroshev <doroshevpetr@gmail.com>
2023-04-03 21:50:06 +0300

    some updates

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@
 using namespace std;
 
 bool is_correct_rule(string& r);
+
 string trim(string& r);
 
 int main(){
 
     Grammar g;
     string rule = " ";
+    int length;
 
     cout << "Введите правила грамматики (конец ввода - пустая строка): " << std::endl;
 
@@ -30,11 +32,27 @@ int main(){
         getline(cin, rule);
     }
 
-    return 0;
+    cout << "Преобразованная грамматика: " << endl;
+    g.simplify();
+    g.print_rules();
+
+    cout << endl;
+    cout << "Введите максимальную длину цепочки: ";
+    cin >> length;
+    cout << endl;
+
+
+    vector<string> words = g.generate_words(length);
+    for (auto& w: words){
+        std::cout << w << std::endl;
+    }
+
+
+    return 0;   
 }
 
 bool is_correct_rule(string& r){
-    return regex_match( r, regex("[A-Z]=[A-Za-z0-9]+(\\|[A-Za-z0-9]+)*") );
+    return regex_match( r, regex("[A-Z]=.+(\\|.+)*") );
 }
 
 string trim(string& r){
